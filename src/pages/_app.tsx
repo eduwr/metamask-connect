48b2313640edcefb905b71f0fad2fe7b93e26cdc
eduwr <eduwric@gmail.com>
2021-10-03 22:09:34 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { DAppProvider } from "@usedapp/core";
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <DAppProvider config={{}}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </DAppProvider>
+  );
+}
+export default MyApp;
