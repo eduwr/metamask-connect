b6ba94afa452e80e08dd04042d12837241b0c747
eduwr <eduwric@gmail.com>
2021-10-03 22:17:25 -0300

    feat: :sparkles: layout component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { DAppProvider } from "@usedapp/core";
+import Layout from "../components/Layout";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <DAppProvider config={{}}>
       <ChakraProvider>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ChakraProvider>
     </DAppProvider>
   );
